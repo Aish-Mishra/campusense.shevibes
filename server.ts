12c0e3a043a27752d4e474cd1391b2a842255578
Rimsha Agrawal <rimshaagrawal.ed@gmail.com>
2026-09-16 01:56:01 +0530

    UPDATE SERVER

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { parseCampusNoticeLocal, answerNoticeQuestionLocal } from "./localNotice
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "10mb" }));
 
